cbdb4f5b03ea8a68b71af023c8a2a18db7d7d99c
Martin Moreno <MARTIN51189@gmail.com>
2025-03-04 20:13:11 +0000

    solved

--- 01_contador_incremental/ejercicio/js/index.js
+++ 01_contador_incremental/ejercicio/js/index.js
@@ -1,3 +1,15 @@
 
 // Escribir una función clousure que 
 // aumente un contador en 1 cada vez que la función se llame
+
+
+function getClousure(valor) {
+    return () => valor +=1;
+}
+
+const contador = getClousure(0);
+
+console.log(contador());
+console.log(contador());
+console.log(contador());
+console.log(contador());
