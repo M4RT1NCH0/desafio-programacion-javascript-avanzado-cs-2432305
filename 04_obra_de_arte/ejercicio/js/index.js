beafcb95fe76034d0276ba36ae37ea44ea6e7742
Martin Moreno <MARTIN51189@gmail.com>
2025-03-06 00:37:48 +0000

    Solved

--- 04_obra_de_arte/ejercicio/js/index.js
+++ 04_obra_de_arte/ejercicio/js/index.js
@@ -2,3 +2,20 @@
 // Crear una clase que cargue una imágen y que se muestre en la página.
 
 const rutaImagen = '../../imagenes/Cat_inside_LIL_134199.jpg';
+
+class Img {
+    _path;
+
+    constructor(path) {
+        this._ruta = path;
+    }
+
+    loadImage(){
+        const img = document.createElement('img');
+        img.src = this._ruta;
+        document.body.appendChild(img);
+    }
+}
+
+const img = new Img(rutaImagen);
+img.loadImage();
